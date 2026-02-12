ab4548f61e3e9917170cf6fc9381147aaa149bb9
tathyadoot-png <tathyadoot@gmail.com>
2026-02-12 13:43:33 +0530

    fixes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+imports: [
+  PrismaModule,
+  AuthModule,
+  UsersModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
